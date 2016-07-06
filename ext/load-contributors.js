f0b1170863070c722113fe8930e9b59019b0a837
Nils Adermann <naderman@naderman.de>
2016-07-06 18:30:23 +0200

    Prevent doube loading of contributor listing

--- ext/load-contributors.js
+++ ext/load-contributors.js
@@ -1,14 +1,16 @@
 window.loadContributors = () => {
     $('.file-header').each((_, header) => {
-        $(header).find('.file-actions a.btn:contains("View")').each((_, anchor) => {
-            $.get($(anchor).attr('href')).done(function (data) {
-                var contributors = $(data).find('.commit-tease-contributors');
-                $(contributors).css({'margin-top': '0px', 'border-top': 'none'});
-                $(contributors).find('.contributors-toggle').contents()[0].textContent = 'File has ';
-                var bar = $('<div class="file-header"></div>').append($(contributors)[0]);
-                $(bar).css('padding-top', '0px');
-                $(header).after($(bar));
+        if (!$(header).next().hasClass("contributor-bar")) {
+            $(header).find('.file-actions a.btn:contains("View")').each((_, anchor) => {
+                $.get($(anchor).attr('href')).done(function (data) {
+                    var contributors = $(data).find('.commit-tease-contributors');
+                    $(contributors).css({'margin-top': '0px', 'border-top': 'none'});
+                    $(contributors).find('.contributors-toggle').contents()[0].textContent = 'File has ';
+                    var bar = $('<div class="file-header contributor-bar"></div>').append($(contributors)[0]);
+                    $(bar).css('padding-top', '0px');
+                    $(header).after($(bar));
+                });
             });
-        });
+        }
     });
 }
