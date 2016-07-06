323709d0cef37f1d28d8229b2ddbff42180a2d91
Richard Wossal <richard@r-wos.org>
2016-07-06 14:07:48 +0200

    github api example

--- ext/main.js
+++ ext/main.js
@@ -1,2 +1,11 @@
 console.log($(document));
-alert("hello");
+
+var GH_TOKEN = localStorage.getItem('GH_TOKEN');
+if (!GH_TOKEN) {
+    alert("do localStorage.setItem('GH_TOKEN', .....token....);");
+}
+
+$.get('https://api.github.com/issues', { access_token: GH_TOKEN })
+    .done(function(data) {
+        console.log(data);
+    });
