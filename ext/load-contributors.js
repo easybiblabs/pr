4d22f0ce53cf34510e41829d0b9765be90e04b17
Nils Adermann <naderman@naderman.de>
2016-07-06 17:30:57 +0200

    Shorten jquery css call

--- ext/load-contributors.js
+++ ext/load-contributors.js
@@ -3,8 +3,7 @@ window.loadContributors = () => {
         $(header).find('.file-actions a.btn:contains("View")').each((_, anchor) => {
             $.get($(anchor).attr('href')).done(function (data) {
                 var contributors = $(data).find('.commit-tease-contributors');
-                $(contributors).css('margin-top', '0px');
-                $(contributors).css('border-top', 'none');
+                $(contributors).css({'margin-top': '0px', 'border-top', 'none'});
                 var bar = $('<div class="file-header"></div>').append($(contributors)[0]);
                 $(bar).css('padding-top', '0px');
                 $(header).after($(bar));
