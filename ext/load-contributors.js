f2a94603a1dec59ab75be8f5aee58197f1bcf439
Nils Adermann <naderman@naderman.de>
2016-07-06 17:39:55 +0200

    Prefix contributor listing in diff with "File has " to avoid confusion

--- ext/load-contributors.js
+++ ext/load-contributors.js
@@ -3,7 +3,8 @@ window.loadContributors = () => {
         $(header).find('.file-actions a.btn:contains("View")').each((_, anchor) => {
             $.get($(anchor).attr('href')).done(function (data) {
                 var contributors = $(data).find('.commit-tease-contributors');
-                $(contributors).css({'margin-top': '0px', 'border-top', 'none'});
+                $(contributors).css({'margin-top': '0px', 'border-top': 'none'});
+                $(contributors).find('.contributors-toggle').contents()[0].textContent = 'File has ';
                 var bar = $('<div class="file-header"></div>').append($(contributors)[0]);
                 $(bar).css('padding-top', '0px');
                 $(header).after($(bar));
