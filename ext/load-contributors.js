7ff81acd89a39e63b944576fa3c7e6e6cf74aec3
Nils Adermann <naderman@naderman.de>
2016-07-06 19:10:00 +0200

    Load contributor modal box

--- ext/load-contributors.js
+++ ext/load-contributors.js
@@ -1,12 +1,19 @@
 window.loadContributors = () => {
-    $('.file-header').each((_, header) => {
+    $('.file-header').each((i, header) => {
         if (!$(header).next().hasClass("contributor-bar")) {
             $(header).find('.file-actions a.btn:contains("View")').each((_, anchor) => {
                 $.get($(anchor).attr('href')).done(function (data) {
+                    var contributorsBox = $(data).find('#blob_contributors_box')
+                    $(contributorsBox).attr('id', 'blob_contributors_box_'+i);
+
                     var contributors = $(data).find('.commit-tease-contributors');
                     $(contributors).css({'margin-top': '0px', 'border-top': 'none'});
                     $(contributors).find('.contributors-toggle').contents()[0].textContent = 'File has ';
-                    var bar = $('<div class="file-header contributor-bar"></div>').append($(contributors)[0]);
+                    $(contributors).find('.contributors-toggle').attr('data-facebox', '#blob_contributors_box_'+i);
+
+                    var bar = $('<div class="file-header contributor-bar"></div>')
+                        .append($(contributors)[0])
+                        .append($(contributorsBox)[0]);
                     $(bar).css('padding-top', '0px');
                     $(header).after($(bar));
                 });
