ee815bbb132a8ed32dd5028edfb1d0aa417af93d
Nils Adermann <naderman@naderman.de>
2016-07-06 17:29:32 +0200

    Move load contributors to its own file as well

---
+++ ext/load-contributors.js
@@ -0,0 +1,14 @@
+window.loadContributors = () => {
+    $('.file-header').each((_, header) => {
+        $(header).find('.file-actions a.btn:contains("View")').each((_, anchor) => {
+            $.get($(anchor).attr('href')).done(function (data) {
+                var contributors = $(data).find('.commit-tease-contributors');
+                $(contributors).css('margin-top', '0px');
+                $(contributors).css('border-top', 'none');
+                var bar = $('<div class="file-header"></div>').append($(contributors)[0]);
+                $(bar).css('padding-top', '0px');
+                $(header).after($(bar));
+            });
+        });
+    });
+}
